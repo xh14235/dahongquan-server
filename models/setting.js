37741cd482105ceb2d40c264edf4fa67c9a870e8
xh14235 <15021161540@163.com>
2024-04-03 09:44:34 +0800

    feat: 增加首页视频上传功能

--- models/setting.js
+++ models/setting.js
@@ -4,6 +4,7 @@ const schema = new mongoose.Schema({
   id: { type: String },
   bgUrl: { type: String },
   logoUrl: { type: String },
+  homeVideo: { type: String },
 });
 
 module.exports = mongoose.model("setting", schema);
